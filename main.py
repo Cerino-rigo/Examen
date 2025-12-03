2e532cc5065a0f2cb4ebd65b1fb4bd0d92a413fa
Rigo-cerino <cerino.rigoberto@tec.mx>
2025-12-02 19:27:56 -0600

    Ajuste

--- main.py
+++ main.py
@@ -1,24 +1,13 @@
 import streamlit as st 
 st.set_page_config(page_title="Data manager", page_icon="📈", layout="wide")
 
-# Sidebar
-#st.sidebar.write(
-#    "<h1 style='text-align: center;'>🎮 Menú del Examen</h1>",
-#    unsafe_allow_html=True
-#)
-
-Home = st.Page("Menu/page1.py", title="Análisis por Usuario", icon=":material/home:")
-Predicciones = st.Page("Menu/page2.py", title= "Análisis General", icon=":material/track_changes:")
 Airbnb = st.Page("Menu/page3.py", title="Resumen General de Airbnb", icon=":material/analytics:")
 
 pg = st.navigation(
         {
-            "MENÚ PRINCIPAL": [Home, Predicciones, Airbnb],
+            "MENÚ PRINCIPAL": [Airbnb],
             
         })
     
-#pg = st.navigation([Home, Predicciones])
 
 pg.run()
-
-#st.title("Página principal")
